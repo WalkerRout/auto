6b7e2cf0346a3f8ca5d9adda6801b8b609d412c8
walkerrout04@gmail.com <walkerrout04@gmail.com>
2025-02-16 17:14:43 -0800

    add simple example

---
+++ examples/simple.rs
@@ -0,0 +1,15 @@
+use auto::Tape;
+
+fn main() {
+  // Create a new tape (Wengert List) to store the nodes of our computation
+  let mut tape = Tape::new();
+  
+  // Define a scope to play around in
+  tape.scope(|guard| {
+    let x = guard.var(2.0);
+    let y = x.sin() + x.cos();
+    // After locking a guard, we can only spawn more subcomputations, or collapse into gradients
+    let grads = guard.lock().collapse().of(&y);
+    println!("Value: {}, dy/dx: {}", *y, grads[&x]);
+  });
+}
